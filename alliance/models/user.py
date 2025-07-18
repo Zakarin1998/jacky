d8f1e3ceeb6287537fbe6612f31e9fdd1003d950
abrillante <abrillante@tecnetdati.it>
2025-07-18 15:36:39 +0200

    del: project moved

--- alliance/models/user.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, profile: dict):
-        self.profile = profile
-
-    def update(self, updates: dict):
-        self.profile.update(updates)
-
-    def to_dict(self):
-        return self.profile
